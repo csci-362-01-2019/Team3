4332c6f6b670851877dbfb1e01feda159268ebfd
RogueKnight2065 <anuszkiewiczmb@g.cofc.edu>
2019-10-17 11:23:48 -0400

    Debugging the testing code.

--- testerOpenAPI.sh
+++ testerOpenAPI.sh
@@ -10,19 +10,21 @@ failCount=0
 #TESTCASES=/test_cases/*
 #for file in TESTCASES   # Main loop reading in files
 #do  # Do for the Main loop
-    for file in test_cases/*.txt; #for reading in contents 
+    for file in openaq-testing/*.txt; #for reading in contents 
     do
     echo "Running: $file"
-        while IFS='\n' read -r testNum testFile component nodeCMD oracle;  #change when get testCases
+        while IFS='\' read -r testNum testFile component nodeCMD oracle;  #change when get testCases
         # while to assign content to variables
         do
+	    echo "$testNum" #testing
+	    echo "$testFile" #testing
             echo "$testNum $testFile $units $nodeCMD $oracle"
         done <"$file"   # done for variable assignment
     done # done for reading in the file
 
 echo "Test: $testNum"
 echo "Testing File: $testFile"
-$nodeCMD #> testingOutputs.txt  #The node command has the needed textFile in it.
+$nodeCMD > testingOutputs.txt  #The node command has the needed textFile in it.
 # grep testing........ 
 if grep -q "$oracle" testingOutputs.txt;
     then passCount++ echo "Test passed"
