00fdfa911e2e2bb7675ab3f1a7cb2803f153aaff
BenjiDuke <54375202+BenjiDuke@users.noreply.github.com>
2019-10-21 20:45:15 +0000

    Add files via upload
    Making the file structure match. The test case files (in testCaseFile) need to be copied into the sources folder before testing

---
+++ TestAutomation/scripts/testDriver.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This is the script for running our project
+
+passCount=0
+failCount=0
+
+#cd .. # cd to Main file structure
+
+#TESTCASES=/test_cases/*
+#for file in TESTCASES   # Main loop reading in files
+#do  # Do for the Main loop
+    for file in test_cases/*.txt; #for reading in contents 
+    do
+    echo "Running: $file"
+        while IFS='\n' read -r testNum testFile component nodeCMD oracle;  #change when get testCases
+        # while to assign content to variables
+        do
+            echo "$testNum $testFile $units $nodeCMD $oracle"
+        done <"$file"   # done for variable assignment
+    done # done for reading in the file
+
+echo "Test: $testNum"
+echo "Testing File: $testFile"
+$nodeCMD #> testingOutputs.txt  #The node command has the needed textFile in it.
+# grep testing........ 
+if grep -q "$oracle" testingOutputs.txt;
+    then passCount++ echo "Test passed"
+#else
+#    then failCount++ echo "Test failed"
+fi
+#done  # Done for the Main loop
