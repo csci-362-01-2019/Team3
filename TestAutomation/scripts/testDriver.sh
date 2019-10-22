8fa33515a909318f9067f53a8c3f4291522a4a7e
RogueKnight2065 <anuszkiewiczmb@g.cofc.edu>
2019-10-22 09:06:34 -0400

    debugging script

--- TestAutomation/scripts/testDriver.sh
+++ TestAutomation/scripts/testDriver.sh
@@ -5,15 +5,15 @@
 passCount=0
 failCount=0
 
-#cd .. # cd to Main file structure
+cd ..
 
 #TESTCASES=/test_cases/*
 #for file in TESTCASES   # Main loop reading in files
 #do  # Do for the Main loop
-    for file in test_cases/*.txt; #for reading in contents 
+    for file in testCases/*.txt; #for reading in contents 
     do
     echo "Running: $file"
-        while IFS='\n' read -r testNum testFile component nodeCMD oracle;  #change when get testCases
+        while IFS='\' read -r testNum testFile component nodeCMD oracle;  #change when get testCases
         # while to assign content to variables
         do
             echo "$testNum $testFile $units $nodeCMD $oracle"
@@ -22,10 +22,12 @@ failCount=0
 
 echo "Test: $testNum"
 echo "Testing File: $testFile"
-$nodeCMD #> testingOutputs.txt  #The node command has the needed textFile in it.
+$nodeCMD > testingOutputs.txt  #The node command has the needed textFile in it.
 # grep testing........ 
 if grep -q "$oracle" testingOutputs.txt;
-    then passCount++ echo "Test passed"
+    then
+	    let  passCount++
+	    echo "Test passed"
 #else
 #    then failCount++ echo "Test failed"
 fi
